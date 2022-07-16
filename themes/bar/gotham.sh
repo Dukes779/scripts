d35759add552acab35d9e06edc21a92930874517
Dukes779 <dunkenvizlo@gmail.com>
2022-07-16 20:47:14 +0200

    gotham

---
+++ themes/bar/gotham.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# colors
+
+blue=#26a98b
+darkblue=#1e8870
+default=#0a0f14
+red=#c33027
+white=#D9E0EE
+grey=#0a0f14
+purple=#888ba5
+darkpurple=#c4a1d6
+yellow=#F9E2AF
+green=#0f5d7a
+pink=#E8A2AF
+darkpink=#D1929E
