99e47d4f587dd2b227f6c747ffdfcbaca95dd6b9
Dukes779 <dunkenvizlo@gmail.com>
2022-09-03 18:11:34 +0200

    everforest

---
+++ themes/bar/everforest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# colors
+
+blue=#87c095
+darkblue=#709F7B
+default=#141816
+red=#e58183
+white=#D9E0EE
+grey=#0a0f14
+purple=#d39bb6
+darkpurple=#c4a1d6
+yellow=#F9E2AF
+green=#83b6af
+pink=#E8A2AF
+darkpink=#D1929E
